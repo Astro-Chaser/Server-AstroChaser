096cd06696d196aacbaba66b7875649e064065ca
Jeonjunhowi <jjh63360@kau.kr>
2022-06-25 23:42:38 +0900

    add: [GET]방명록 조회 API

--- src/app/guestbook/guestBookDao.js
+++ src/app/guestbook/guestBookDao.js
@@ -3,16 +3,28 @@ async function createGuestBook(connection, createGuestBookParams){
     const createGuestBookQuery=`
                                 insert into GuestBook(writer, content)
                                 VALUES (?, ?);
-                                `
-    console.log("hihi");                  
+                                `                 
     const createGuestBookRow = await connection.query(
         createGuestBookQuery,
         createGuestBookParams
     );
-    
+
     return createGuestBookRow;
 }
 
+//방명록 반환
+async function getGuestbook(connection){
+    const getGuestbookQuery = `select writer, content, createdAt
+                                from GuestBook
+                                where state='A';`
+
+    const [getGuestbookRow] = await connection.query(getGuestbookQuery);
+
+    return getGuestbookRow;
+}
+
+
 module.exports = {
-    createGuestBook
+    createGuestBook,
+    getGuestbook
 }
