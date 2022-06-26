2e815c7d38280c6a157a6c3bfb8265bd36637652
Jeonjunhowi <jjh63360@kau.kr>
2022-06-26 20:52:55 +0900

    add: guestbook Dao

---
+++ src/app/guestbook/guestbookDao.js
@@ -0,0 +1,30 @@
+//방명록 작성
+async function createGuestBook(connection, createGuestBookParams){
+    const createGuestBookQuery=`
+                                insert into GuestBook(writer, content)
+                                VALUES (?, ?);
+                                `                 
+    const createGuestBookRow = await connection.query(
+        createGuestBookQuery,
+        createGuestBookParams
+    );
+
+    return createGuestBookRow;
+}
+
+//방명록 조회
+async function getGuestbook(connection){
+    const getGuestbookQuery = `select writer, content, createdAt
+                                from GuestBook
+                                where state='A';`
+
+    const [getGuestbookRow] = await connection.query(getGuestbookQuery);
+
+    return getGuestbookRow;
+}
+
+
+module.exports = {
+    createGuestBook,
+    getGuestbook
+}
