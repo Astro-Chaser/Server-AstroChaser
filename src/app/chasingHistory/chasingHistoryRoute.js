f4d84f0ef6d402bec069c2280f49799af3e545f7
Jeonjunhowi <jjh63360@kau.kr>
2022-07-17 00:49:32 +0900

    add: 사진 게시판 타이틀 가져오기 API

--- src/app/chasingHistory/chasingHistoryRoute.js
+++ src/app/chasingHistory/chasingHistoryRoute.js
@@ -1,3 +1,6 @@
 module.exports = function(app){
-    //const chasingHistoryController = 
+    const chasingHistory = require('./chasingHistoryController');
+
+    //1. 게시판 타이틀 가져오기 API
+    app.get('/app/picture-board/title', chasingHistory.getChasingHistoryTitle);
 }
