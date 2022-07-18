7cfc11df7d5369b54148f80e88da81c977e0078b
Jeonjunhowi <jjh63360@kau.kr>
2022-07-18 12:56:28 +0900

    add: 운영진 확인

--- frontWEB/static/js/pictureNoticeBoard.js
+++ frontWEB/static/js/pictureNoticeBoard.js
@@ -1,7 +1,20 @@
+const pictureWriteBtn = document.getElementById("picture-write-btn");
+
+pictureWriteBtn.onclick = pictureWriteBtnClicked;
 
 window.onload = async function(){
     showPictureNoticeBoard();
+}
 
+async function pictureWriteBtnClicked(event){
+    if(localStorage.getItem("member")=="운영진")
+    {
+        alert("운영진");
+    }
+    else
+    {
+        alert("운영진만 글을 작성할 수 있는 게시판입니다.")
+    }
 }
 
 async function showPictureNoticeBoard(){
@@ -77,4 +90,4 @@ async function getAPI(host, path, headers = {}) {
     } else {
       throw new Error(data);
     }
-  }
+}
