d9b68cff59648e43167b25dad14f2780333e6212
Jeonjunhowi <jjh63360@kau.kr>
2022-07-19 14:51:15 +0900

    add: TEST picture s3 bucket upload

--- src/app/chasingHistory/chasingHistoryRoute.js
+++ src/app/chasingHistory/chasingHistoryRoute.js
@@ -1,6 +1,12 @@
 module.exports = function(app){
     const chasingHistory = require('./chasingHistoryController');
+    const {imageUploader} = require('./s3ImgUploader');
 
     //1. 게시판 타이틀 가져오기 API
     app.get('/app/picture-board/title', chasingHistory.getChasingHistoryTitle);
+
+    //2. 게시판 사진 업로드 API
+    app.post('/test/image', imageUploader.single('image'), (req, res) => {
+        res.send('good!')
+      })
 }
---
+++ src/app/chasingHistory/s3ImgUploader.js
@@ -0,0 +1,36 @@
+const AWS = require('aws-sdk');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+const path = require('path');
+const secret_config = require("../../../config/secret");
+
+AWS.config.update({
+    region: 'ap-northeast-2',
+    accessKeyId: secret_config.accessKeyId,
+    secretAccessKey: secret_config.secretAccessKey,
+  });
+  
+const s3 = new AWS.S3()
+
+const allowedExtensions = ['.png', '.jpg', '.jpeg', '.bmp']
+
+const imageUploader = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: 'astrochaser', // 생성한 버킷 이름을 적어주세요.
+    key: (req, file, callback) => {
+      const uploadDirectory = req.query.directory ?? '' // 업로드할 디렉토리를 설정하기 위해 넣어둔 코드로, 없어도 무관합니다.
+      const extension = path.extname(file.originalname)
+      if (!allowedExtensions.includes(extension)) { // extension 확인을 위한 코드로, 없어도 무관합니다.
+        return callback(new Error('wrong extension'))
+      }
+      callback(null, `${uploadDirectory}/${Date.now()}_${file.originalname}`)
+    },
+    acl: 'public-read-write'
+  }),
+})
+
+  
+module.exports ={
+    imageUploader
+} 
