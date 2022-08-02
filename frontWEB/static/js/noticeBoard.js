6b0c2bbe0f5ff4267c8ec407c09e50ed20f7d307
Jeonjunhowi <jjh63360@kau.kr>
2022-08-02 20:03:27 +0900

    add: 일반 게시판 목록형 뷰 구현

--- frontWEB/static/js/noticeBoard.js
+++ frontWEB/static/js/noticeBoard.js
@@ -1,40 +1,83 @@
-window.onload = function(){
-    showNormalNoticeBoardTitles();
+let noticeBoardPage = 0;
+const noticePrevBtn = document.getElementById("notice-prev-btn");
+const noticeNextBtn = document.getElementById("notice-next-btn");
+let titleArr;
+
+window.onload = async function(){
+    titleArr = await getNormalNoticeBoardTitles();
+    showNormalNoticeBoardTitles(noticeBoardPage);
 }
 
-async function showNormalNoticeBoardTitles(){
-    const getTitleRes = await getAPI(hostAddress, 'app/notice/title');
+async function showNormalNoticeBoardTitles(page){
     html = ''
-    for(var i in getTitleRes.result){
-        console.log
-        html += `
-        <div class="columns">
-            <div class="column is-full">
-                <div class="noticeCol iconCol"></div>
-                <div class="noticeCol titleCol">${getTitleRes.result[i].title}</div>
-                <div class="noticeCol writerCol">${getTitleRes.result[i].name}</div>
-                <div class="noticeCol timeCol">${getTitleRes.result[i].createdat}</div>
-                <div class="noticeCol watchCol">${getTitleRes.result[i].viewCount}</div>
-            </div>
-        </div>
-        `
-        if(i==10) {
+
+    for(var i = 0; i<10; i++){
+        if(i==9 || Number((page*10)+i) == titleArr.length-1 ) {
             html += `
+                <div class="removeColumns">
                     <div class="columns">
                         <div class="column is-full" id="final-column">
                             <div class="noticeCol iconCol"></div>
-                            <div class="noticeCol titleCol">${getTitleRes.result[i].title}</div>
-                            <div class="noticeCol writerCol">${getTitleRes.result[i].name}</div>
-                            <div class="noticeCol timeCol">${getTitleRes.result[i].createdat}</div>
-                            <div class="noticeCol watchCol">${getTitleRes.result[i].viewCount}</div>
+                            <div class="noticeCol titleCol">${titleArr[Number((page*10)+i)].title}</div>
+                            <div class="noticeCol writerCol">${titleArr[Number((page*10)+i)].name}</div>
+                            <div class="noticeCol timeCol">${titleArr[Number((page*10)+i)].createdat.substring(0, 10)}</div>
+                            <div class="noticeCol watchCol">${titleArr[Number((page*10)+i)].viewCount}</div>
                         </div>
                     </div>
+                </div>
                 `
             $('.notice-list').append(html)
             break;
         }
+        html += `
+        <div class="removeColumns">
+            <div class="columns">
+                <div class="column is-full">
+                    <div class="noticeCol iconCol"></div>
+                    <div class="noticeCol titleCol">${titleArr[Number(page*10 + i)].title}</div>
+                    <div class="noticeCol writerCol">${titleArr[Number(page*10 + i)].name}</div>
+                    <div class="noticeCol timeCol">${titleArr[Number(page*10 + i)].createdat.substring(0, 10)}</div>
+                    <div class="noticeCol watchCol">${titleArr[Number(page*10 + i)].viewCount}</div>
+                </div>
+            </div>
+        </div>
+        `
        
     }      
+}
+
+noticePrevBtn.onclick = function(){
+    if(noticeBoardPage>=1){
+        $('.removeColumns').empty();
+        Number(noticeBoardPage--);
+        showNormalNoticeBoardTitles(noticeBoardPage);
+    }
+}
+noticeNextBtn.onclick = function(){
+    if(titleArr.length/10 > noticeBoardPage+1)
+    {
+        $('.removeColumns').empty();
+        Number(noticeBoardPage++);
+        console.log(noticeBoardPage)
+        showNormalNoticeBoardTitles(noticeBoardPage);
+    }
+}
+
+async function getNormalNoticeBoardTitles(){
+    const getTitleRes = await getAPI(hostAddress, 'app/notice/title');
+    let titleArr = new Array();
+    for(var i in getTitleRes.result){
+        let titleRes = new Object();
+        titleRes.title = getTitleRes.result[i].title;
+        titleRes.name = getTitleRes.result[i].name;
+        titleRes.createdat = getTitleRes.result[i].createdat;
+        titleRes.viewCount = getTitleRes.result[i].viewCount;
+
+        titleArr.push(titleRes);
+    }
+
+    return titleArr;
+
 }
 
 
