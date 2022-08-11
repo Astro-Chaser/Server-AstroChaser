846ec6ad885928de26442abbd18e4251b9a7770e
Jeonjunhowi <jjh63360@kau.kr>
2022-08-12 01:44:26 +0900

    Add: 자유게시판 추가

--- index.js
+++ index.js
@@ -75,6 +75,7 @@ app.get('/notice/editor', function(req, res){
 app.get('/notice/:pageNum', function(req, res){
     res.sendFile(__dirname + "/frontWEB/noticeBoardContent.html");
 })
+
 app.get('/chasing/notice', function(req, res){
     res.sendFile(__dirname + "/frontWEB/chasingNoticeBoard.html");
 })
@@ -85,6 +86,15 @@ app.get('/chasing/notice/:pageNum', function(req, res){
     res.sendFile(__dirname + "/frontWEB/chasingNoticeBoardContent.html");
 })
 
+app.get('/free-board', function(req, res){
+    res.sendFile(__dirname + "/frontWEB/userFreeBoard.html");
+})
+app.get('/free-board/editor', function(req, res){
+    res.sendFile(__dirname + "/frontWEB/userFreeBoardEditor.html");
+})
+app.get('/free-board/:pageNum', function(req, res){
+    res.sendFile(__dirname + "/frontWEB/userFreeBoardContent.html");
+})
 
 //5. 회칙 페이지
 app.get('/rules', function(req, res){
