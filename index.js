fd4dbff6f3fb56ff5f80969abdb9dc1dfc453763
Jeonjunhowi <jjh63360@kau.kr>
2022-08-30 12:47:28 +0900

    add: history page & title

--- index.js
+++ index.js
@@ -140,6 +140,10 @@ app.get('/astro-event', function(req, res){
     res.sendFile(__dirname + "/frontWEB/astroEvents.html");
 })
 
+//7. 연혁 페이지
+app.get('/history', function(req, res){
+    res.sendFile(__dirname + "/frontWEB/history.html");
+})
 // express 서버를 실행할 때 필요한 포트 정의 및 실행 시 callback 함수를 받습니다
 app.listen(port, function() {
     console.log('start! express server');
